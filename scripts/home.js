338e3add23cdf1ca07ae833c75ffe31e4216db67
Toginomi <rudyiipilande@gmail.com>
2025-02-12 11:52:10 +0800

    commit

--- scripts/home.js
+++ scripts/home.js
@@ -98,6 +98,9 @@ document.addEventListener("DOMContentLoaded", function () {
             `;
 
             courseListElement.appendChild(courseDiv);
+            courseDiv.addEventListener('click', () => {
+                displayCourseDetails(course);
+            });
         });
 
         const totalCredits = filteredCourses.reduce((total, course) => total + course.credits, 0);
@@ -129,4 +132,25 @@ document.addEventListener("DOMContentLoaded", function () {
             hamburgerMenu.textContent = "☰";
         }
     });
+
+    const courseDetails = document.querySelector("#course-details");
+
+    function displayCourseDetails(course) {
+        courseDetails.innerHTML = '';
+        courseDetails.innerHTML = `
+            <button id="closeModal">❌</button>
+            <h2>${course.subject} ${course.number}</h2>
+            <h3>${course.title}</h3>
+            <p><strong>Credits</strong>: ${course.credits}</p>
+            <p><strong>Certificate</strong>: ${course.certificate}</p>
+            <p>${course.description}</p>
+            <p><strong>Technologies</strong>: ${course.technology.join(', ')}</p>
+            `;
+        courseDetails.showModal();
+
+        closeModal.addEventListener("click", () => {
+            courseDetails.close();
+        });
+    }
 });
+
